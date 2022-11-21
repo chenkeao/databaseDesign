8b94d5366f0d169b57218918234e6fb4e61156cf
chenkeao <efkeok@gmail.com>
2022-11-21 22:39:16 +0800

    增加了数据

---
+++ sql/学院出版社数据.sql
@@ -0,0 +1,52 @@
+use material_manage;
+
+insert into 出版社 (id, 名称, 地址, 联系电话)
+values (id, '现代教育出版社', NULL, NULL);
+insert into 出版社 (id, 名称, 地址, 联系电话)
+values (id, '电子工业出版社', NULL, NULL);
+insert into 出版社 (id, 名称, 地址, 联系电话)
+values (id, '清华大学出版社', NULL, NULL);
+insert into 出版社 (id, 名称, 地址, 联系电话)
+values (id, '高等教育出版社', NULL, NULL);
+insert into 出版社 (id, 名称, 地址, 联系电话)
+values (id, '华南理工大学出版社', NULL, NULL);
+insert into 出版社 (id, 名称, 地址, 联系电话)
+values (id, '国防工业出版社', NULL, NULL);
+insert into 出版社 (id, 名称, 地址, 联系电话)
+values (id, '机械工业出版社', NULL, NULL);
+insert into 出版社 (id, 名称, 地址, 联系电话)
+values (id, '化学工业出版社', NULL, NULL);
+insert into 出版社 (id, 名称, 地址, 联系电话)
+values (id, '人民邮电出版社', NULL, NULL);
+insert into 出版社 (id, 名称, 地址, 联系电话)
+values (id, '中国电力出版社', NULL, NULL);
+
+select * from 出版社;
+
+
+insert into 学院 values (id,'材料科学与工程学院');
+insert into 学院 values (id,'智能制造学院');
+insert into 学院 values (id,'电气工程与自动化学院');
+insert into 学院 values (id,'计算机与信息工程学院/软件学院');
+insert into 学院 values (id,'土木工程学院');
+insert into 学院 values (id,'环境工程与化学学院');
+insert into 学院 values (id,'经济与管理学院');
+insert into 学院 values (id,'会计学院');
+insert into 学院 values (id,'艺术设计学院');
+insert into 学院 values (id,'外国语学院');
+insert into 学院 values (id,'人文与社会科学学院');
+insert into 学院 values (id,'教育科学与音乐学院');
+insert into 学院 values (id,'马克思主义学院');
+insert into 学院 values (id,'继续教育学院');
+
+
+insert into 班级 values (id,'19计科1班',2019,4);
+insert into 班级 values (id,'19计科2班',2019,4);
+insert into 班级 values (id,'19计科3班',2019,4);
+insert into 班级 values (id,'19软工1班',2019,4);
+insert into 班级 values (id,'19软工2班',2019,4);
+insert into 班级 values (id,'19软工3班',2019,4);
+insert into 班级 values (id,'19大数据1班',2019,4);
+insert into 班级 values (id,'19大数据2班',2019,4);
+insert into 班级 values (id,'19大数据3班',2019,4);
+
---
+++ sql/班级课程数据.sql
@@ -0,0 +1,12 @@
+insert into 班级课程表 (班级ID, 课程ID, 学年, 学期)
+values (1,186,2022,1);
+insert into 班级课程表 (班级ID, 课程ID, 学年, 学期)
+values (1,187,2022,1);
+insert into 班级课程表 (班级ID, 课程ID, 学年, 学期)
+values (1,189,2022,1);
+insert into 班级课程表 (班级ID, 课程ID, 学年, 学期)
+values (1,190,2022,1);
+insert into 班级课程表 (班级ID, 课程ID, 学年, 学期)
+values (1,191,2022,1);
+insert into 班级课程表 (班级ID, 课程ID, 学年, 学期)
+values (1,193,2022,1);
